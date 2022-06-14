33f8a089701404207bcafd27f791d30a7624b1ee
Tomaszius <liutkus.to@gmail.com>
2022-06-14 14:19:26 +0300

    updated masyvai ciklai

--- pvz-2-ciklai/script.js
+++ pvz-2-ciklai/script.js
@@ -6,11 +6,20 @@ window.onload = function() {
 
     isvedamasyva(pazymiai);
 
-    
     let suma = masyvosuma(pazymiai);
 
     console.log(suma);
     
+    issaugotiLyginiusNuo200iki360();
+
+    const lyginiaiSkaiciai = issaugotiLyginiusNuo200iki360();
+    // isvedamasyva(lyginiaiSkaiciai);
+    console.log(masyvosuma(lyginiaiSkaiciai));
+
+    console.log("-----------------")
+
+    const lyginiaiSkaiciai2 = issaugotilyginiusnuo205iki379();
+    isvedamasyva(lyginiaiSkaiciai2);
 }
 
 
@@ -32,4 +41,32 @@ function masyvosuma(masyvas){
     }
     return suma;
 
+}
+
+function issaugotiLyginiusNuo200iki360(){
+    let masyvas = [];
+
+    for (let i = 200; i <= 360; i += 2){
+            masyvas.push(i);
+    }
+    return masyvas;
+
+
+}
+
+
+// efektyvesnis būdas
+
+function issaugotilyginiusnuo205iki379(){
+    const masyvas = [];
+    for (let i=205; i <= 379; i++){
+        if (arLyginis(i)){
+            masyvas.push(i);
+        }
+    }
+    return masyvas;
+}
+
+function arLyginis(x){
+    return x % 2 === 0;
 }
